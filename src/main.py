fb4e275a9c4b2b5ea3bfd6f4e1c813d2c81072ce
Gabriel-Paes <paessouzagabriel@gmail.com>
2024-04-18 09:39:04 -0300

    Adding imports and list of instances

--- src/main.py
+++ src/main.py
@@ -1,1 +1,26 @@
-print("Hello, World!")
+import os
+import sys
+
+root = os.getcwd()
+sys.path.insert(0, root)
+
+datasetsPath = os.path.join(root,"datasets")
+
+datasets = os.listdir(datasetsPath)
+
+print("Instâncias disponíveis:")
+for index, file in enumerate(datasets):
+    print(f"{index} - {file}")
+
+selectedInstance = int(input("Escolha uma instância: "))
+
+selectedDataset = datasets[selectedInstance]
+
+with open(os.path.join(datasetsPath, selectedDataset)) as file:
+    content = file.read()
+
+print(content)
+
+from algorithms.genetic import function
+
+function()
